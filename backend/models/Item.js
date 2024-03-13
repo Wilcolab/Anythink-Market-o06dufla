fc0ea2f53cf29c55bf5908ca34f9821e1017ca63
dorelb175 <64021257+dorelb175@users.noreply.github.com>
2024-03-13 22:43:42 +0000

    Add isVerified field to seller of items

--- backend/models/Item.js
+++ backend/models/Item.js
@@ -56,7 +56,6 @@ ItemSchema.methods.toJSONFor = function(user) {
     favorited: user ? user.isFavorite(this._id) : false,
     favoritesCount: this.favoritesCount,
     seller: this.seller.toProfileJSONFor(user),
-    isVerified: user.isVerified,
   };
 };
 
